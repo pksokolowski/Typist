2c03dc4bf4c7a7faad2d7ea3cf1d4a4998d22d77
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2019-01-06 20:17:03 +0100

    recognize additional keys as word endings
    Included '?' and '!' recognition.

--- Typist/WordsInterceptor.cs
+++ Typist/WordsInterceptor.cs
@@ -16,7 +16,7 @@ namespace Typist
     {
         private KeyboardHook hook;
 
-        private List<string> WORD_ENDING_KEYS = new List<string> { "Return", "Space", "OemPeriod", "Oemcomma" };
+        private List<string> WORD_ENDING_KEYS = new List<string> { "Return", "Space", "OemPeriod", "Oemcomma", "OemQuestion", "D1" };
 
         private const string KEY_BACKSPACE = "Back";
 
